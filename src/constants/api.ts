41c7d07606f18239d8ff908055cab75c79e9312f
pj-wise <pj-wise>
2022-11-06 02:27:53 -0500

    env in pipeline

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,11 +1,7 @@
 import googleAPI from './constants';
 
-// env was working locally but was having issues with netlify not allowing it and causing build fails.
-// const API_KEY = process.env.REACT_APP_API_KEY;
-// note: this would typically be included as a env in the CI/CD pipeline but for the sake of
-// this assesment I'm leaving it here
+const API_KEY = process.env.REACT_APP_API_KEY;
 
-const API_KEY = 'AIzaSyBYY_xLDK6bYFKqEJU5-7Hy7oKt5p8ZEf0';
 export const API = {
   searchBooks: (queryString: string) =>
     `${googleAPI}/volumes?q=${queryString}&key=${API_KEY}&maxResults=18&fields=items(id,volumeInfo(title, subtitle, categories, publishedDate, authors, description, infoLink, pageCount, previewLink, language, imageLinks))`,
