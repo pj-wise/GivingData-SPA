a7799fb8031ae9ecc1411bceb0a018bf7f0d1120
pj-wise <pj-wise>
2022-11-06 02:23:28 -0500

    update

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -3,7 +3,7 @@ import googleAPI from './constants';
 // env was working locally but was having issues with netlify not allowing it and causing build fails.
 // const API_KEY = process.env.REACT_APP_API_KEY;
 // note: this would typically be included as a env in the CI/CD pipeline but for the sake of
-// this assesment I'm leaving it here so a new api key wont be necessary.
+// this assesment I'm leaving it here
 
 const API_KEY = 'AIzaSyBYY_xLDK6bYFKqEJU5-7Hy7oKt5p8ZEf0';
 export const API = {
